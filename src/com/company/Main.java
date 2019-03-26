f075ac60d477f4de36b0f1039a54e113142f7f08
hamzatz <hamzatzaitov@gmail.com>
2019-03-26 17:21:09 +0600

    new project

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int Golf = 2500;
+        if (Golf == 3000)
+            System.out.println("Да эта машина стоит " + Golf + "$");
+        else{
+        }
+        System.out.println("Нет эта машина не стоит "  + Golf + "$");
+    }
+}
+
+
